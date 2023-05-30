71f5312b0871946d6585e69b1a33d7a86a63c423
Ubinquitous <ubinquitous1@gmail.com>
2023-05-30 17:40:57 +0900

    feat(apis): add interceptor and refresh token logic

---
+++ src/apis/token/refreshToken.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import Storage from "@/apis/storage";
+
+const refreshToken = async () => {
+  try {
+    const res = (
+      await axios.put("/auth/refresh/access", {
+        refresh_token: Storage.getItem("refresh_token"),
+      })
+    ).data;
+    Storage.setItem("access_token", res.accessToken);
+  } catch (err) {
+    console.log(err);
+    Storage.delItem("refresh_token");
+  }
+};
+
+export default refreshToken;
