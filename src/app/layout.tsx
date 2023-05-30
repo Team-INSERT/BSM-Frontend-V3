716c163661d6e76e42e6488a11e07c545ef6e6a6
Ubinquitous <ubinquitous1@gmail.com>
2023-05-30 17:41:53 +0900

    style(app): default setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import "./globals.css";
+import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "부산소프트웨어마이스터고등학교 학생 관리 서비스",
+  title: "BSM",
   description: "부산소마고 학생 정보 관리 서비스입니다.",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import HomePage from "@/pages/home";
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return <HomePage />;
 };
 
 export default Home;
