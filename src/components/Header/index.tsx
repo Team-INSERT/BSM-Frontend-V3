2dcc7db7b2117626d457b4040f182b68c323731a
Ubinquitous <ubinquitous1@gmail.com>
2023-05-30 17:44:45 +0900

    chore(package, directory): add package, change directory architecture

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  width: 100vw;
+  height: 72px;
+`;
+
+export default Header;
