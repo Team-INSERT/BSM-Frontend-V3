6f0d7d34004d28a1083a94f28dd314e200cee37a
Ubinquitous <ubinquitous1@gmail.com>
2023-05-31 23:07:26 +0900

    feat(home): home page container setting

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,15 @@
+import color from "@/styles/color";
 import React from "react";
+import styled from "styled-components";
 
 const HomePage = () => {
-  return <div></div>;
+  return <Container></Container>;
 };
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${color.container};
+`;
 
 export default HomePage;
